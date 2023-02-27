c50450b325417bae3da1f1f33d7ebf10c717c075
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-02-27 09:28:42 +0300

    added database for cards

---
+++ js/database.js
@@ -0,0 +1,56 @@
+let notes = [
+    {
+        id: "card0",
+        group: "Design",
+        title: "Website redesign",
+        text: "Modify the contents of the main website...",
+        date: 05-08-2022,
+        priority: "low",
+        assigned: ["Anton Mayer", "Horst Buddler", "Ibrahimovic Zoltan"]
+    },
+    {
+        id: "card1",
+        group: "Sales",
+        title: "Call potential clients with a bananaphone",
+        text: "Make the product presentation to prospective buyers",
+        date: 06-07-2022,
+        priority: "urgent",
+        assigned: ["Anton Mayer", "Horst Buddler", "Ibrahimovic Zoltan"]
+    },
+    {
+        id: "card2",
+        group: "Backoffice",
+        title: "Accounting invoices",
+        text: "Write open invoices for customer",
+        date: 22-07-2022,
+        priority: "medium",
+        assigned: ["Anton Mayer", "Horst Buddler", "Ibrahimovic Zoltan"]
+    },
+    {
+        id: "card3",
+        group: "Media",
+        title: "Video cut",
+        text: "Edit the new company video",
+        date: 06-09-2022,
+        priority: "medium",
+        assigned: ["Anton Mayer"]
+    },
+    {
+        id: "card4",
+        group: "Media",
+        title: "Video cut 2",
+        text: "Reedit the video",
+        date: 06-10-2022,
+        priority: "medium",
+        assigned: ["Anton Mayer"]
+    },
+    {
+        id: "card5",
+        group: "Marketing",
+        title: "Social media strategy",
+        text: "Develop an ad campaign for brand positioning",
+        date: 06-07-2022,
+        priority: "low",
+        assigned: ["Anton Mayer", "Horst Buddler"]
+    }
+];
