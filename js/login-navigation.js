f3f608089658176f5ed6ee419bbb15b3fe919aca
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-02-28 20:32:25 +0300

    implemented login messages with animation

--- js/login-navigation.js
+++ js/login-navigation.js
@@ -24,3 +24,24 @@ function hidePages() {
         canvas.classList.add('d-none');
     });
 }
+
+/**
+ * Dummy function that needs to be modified, to handle user-input and send actual email.
+ */
+function sendMessage(type) {
+    if (type == "messageReset") {
+        let message = document.getElementById('messageReset');
+        message.classList.add('animate');
+        setTimeout(() => {
+            openPage('login');
+            message.classList.remove('animate');
+        }, 3000);
+    } else {
+        let message = document.getElementById('messageMail');
+        message.classList.add('animate');
+        setTimeout(() => {
+            openPage('resetpw');
+            message.classList.remove('animate');
+        }, 3000);
+    }
+}
