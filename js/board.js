2bbc394c769c886ef2ff19d135771820b9671d26
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-03-01 21:29:53 +0300

    finished summary automatisation

--- js/board.js
+++ js/board.js
@@ -1,3 +1,6 @@
+const monthNames = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"];
+
 function init() {
     renderCards();
     updateCounters()
@@ -312,15 +315,95 @@ function openAddtaskOverlay() {
 
 
 function updateCounters() {
+    countTasks();
+    countProgress();
+    countFeedback();
+    countUrgent();
+    showUpcomingDeadline();
+    countToDo();
+    countDone();
+}
+
+
+function countTasks() {
     let tasksCounter = document.getElementById('tasksCounter');
     tasksCounter.innerHTML = "";
     tasksCounter.innerHTML = cards.length;
+}
 
+
+function countProgress() {
     let progressCounter = document.getElementById('progressCounter');
     progressCounter.innerHTML = "";
     progressCounter.innerHTML = cards.filter(card => card.category == "In Progress").length;
+}
 
+
+function countFeedback() {
     let feedbackCounter = document.getElementById('feedbackCounter');
     feedbackCounter.innerHTML = "";
-    feedbackCounter.innerHTML = cards.filter(card => card.category == "Awaiting Feedback").length
+    feedbackCounter.innerHTML = cards.filter(card => card.category == "Awaiting Feedback").length;
+}
+
+
+function countUrgent() {
+    let urgentCounter = document.getElementById('urgentCounter');
+    urgentCounter.innerHTML = "";
+    urgentCounter.innerHTML = cards.filter(card => card.priority == "urgent").length;
+}
+
+
+function showUpcomingDeadline() {
+    let urgentDate = document.getElementById('urgentDate');
+    urgentDate.innerHTML = "";
+    urgentDate.innerHTML = returnDeadline();
+}
+
+
+/**
+ * This functon filters cards with prority "urgent" and if more than 1
+ * urgent card exists, it hands over the list of cards to the "mostUrgentDeadline()"
+ * function.
+ * @returns String containing month, day and year.
+ */
+function returnDeadline() {
+    let urgentDeadlines = cards.filter(card => card.priority == "urgent");
+    if (urgentDeadlines.length >= 1) {
+        let month = mostUrgentDeadline(urgentDeadlines).getMonth();
+        let day = mostUrgentDeadline(urgentDeadlines).getDate();
+        let year = mostUrgentDeadline(urgentDeadlines).getFullYear();
+        return monthNames[month] + " " + day + ", " +  year;
+    }
+    if (urgentDeadlines.length == 0) {
+        return "No urgent";
+    }
+}
+
+
+/**
+ * 
+ * @param {array} urgentDeadlines containing filtered cards with priority "urgent".
+ * @returns the first date of a ascending sorted list of dates.
+ */
+function mostUrgentDeadline(urgentDeadlines) {
+    let urgentDatesList = [];
+    urgentDeadlines.forEach(urgent => {
+        urgentDatesList.push(urgent.date);
+    });
+    urgentDatesList.sort((a,b) => a-b);
+    return urgentDatesList[0];
+}
+
+
+function countToDo() {
+    let todoCounter = document.getElementById('todoCounter');
+    todoCounter.innerHTML = "";
+    todoCounter.innerHTML = cards.filter(card => card.category == "To do").length;
+}
+
+
+function countDone() {
+    let doneCounter = document.getElementById('doneCounter');
+    doneCounter.innerHTML = "";
+    doneCounter.innerHTML = cards.filter(card => card.category == "Done").length;
 }
--- js/database.js
+++ js/database.js
@@ -6,7 +6,7 @@ let cards = [
         group: "Design",
         title: "Website redesign",
         text: "Modify the contents of the main website...",
-        date: "05.08.2022",
+        date: new Date("05.08.2022"),
         priority: "low",
         assigned: ["Anton Mayer", "Horst Buddler", "Ibrahimovic Zoltan", "Dieter Müller"],
         subtask: [{ text: "subtask 1", status: 1 }, { text: "subtask 2", status: 0 }]
@@ -18,7 +18,7 @@ let cards = [
         group: "Sales",
         title: "Call potential clients with a bananaphone",
         text: "Make the product presentation to prospective buyers",
-        date: "06.07.2022",
+        date: new Date("08/07/2023"),
         priority: "urgent",
         assigned: ["Anton Mayer", "Horst Buddler", "Ibrahimovic Zoltan"],
         subtask: []
@@ -30,8 +30,8 @@ let cards = [
         group: "Backoffice",
         title: "Accounting invoices",
         text: "Write open invoices for customer",
-        date: "22.07.2022",
-        priority: "medium",
+        date: new Date("07.07.2023"),
+        priority: "urgent",
         assigned: ["Anton Mayer", "Horst Buddler", "Ibrahimovic Zoltan"],
         subtask: []
     },
@@ -42,19 +42,19 @@ let cards = [
         group: "Media",
         title: "Video cut",
         text: "Edit the new company video",
-        date: "06.09.2022",
+        date: new Date("07.08.2022"),
         priority: "medium",
         assigned: ["Anton Mayer"],
         subtask: []
     },
     {
         id: "card4",
-        category: "Awaiting Feedback",
+        category: "Done",
         color: "#29ABE2",
         group: "Media",
         title: "Video cut 2",
         text: "Reedit the video",
-        date: "06.10.2022",
+        date: new Date("06.10.2022"),
         priority: "medium",
         assigned: ["Anton Mayer"],
         subtask: []
@@ -66,7 +66,7 @@ let cards = [
         group: "Marketing",
         title: "Social media strategy",
         text: "Develop an ad campaign for brand positioning",
-        date: "06.07.2022",
+        date: new Date("06.07.2022"),
         priority: "low",
         assigned: ["Anton Mayer", "Horst Buddler"],
         subtask: [{ text: "subtask 1", status: 1 }, { text: "subtask 2", status: 1 }, { text: "subtask 3", status: 1 }]
