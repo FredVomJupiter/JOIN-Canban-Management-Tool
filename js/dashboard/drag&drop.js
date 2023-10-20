4bcbd0fb3200c2d93ad96b984951cdacdc33c19e
Frederic Rieg <fredericrieg@protonmail.com>
2023-10-20 12:53:46 +0300

    implemented check & alert for drag&drop

--- js/dashboard/drag&drop.js
+++ js/dashboard/drag&drop.js
@@ -3,10 +3,16 @@
  * Dropzones are below tasks (light gray shade) and the tasks themselves.
  */
 
-let draggedTaskId;
+let draggedTaskId = null;
+let alertTriggered = false;
 
+/**
+ * Runs when a task is dragged. Only the owner of the task can move it.
+ * @param {*} event 
+ * @param {*} taskId 
+ */
 function dragstart(event, taskId) {
-    draggedTaskId = taskId;
+    checkPermission(taskId) ? draggedTaskId = taskId : showAlert();
 }
 
 
@@ -18,6 +24,7 @@ function dragend(event, status) {
             renderTasks();
         }
     });
+    draggedTaskId = null;
 }
 
 
@@ -27,6 +34,7 @@ function dragover(event) {
 
 
 function dragenter(event, zone) {
+    if (draggedTaskId == null) return;
     event.preventDefault();
     let dropzone = document.getElementById(zone);
     dropzone.style.border = "2px dotted black";
@@ -37,4 +45,26 @@ function dragleave(event, zone) {
     event.preventDefault();
     let dropzone = document.getElementById(zone);
     dropzone.style.border = "unset";
+}
+
+/**
+ * Checkes if the logged user is the owner of the task.
+ * @param {*} taskid 
+ * @returns true or false.
+ */
+function checkPermission(taskid) {
+    return tasks.find(task => task.id == taskid).user.id == loggedUser.id;
+}
+
+
+function showAlert() {
+    if (alertTriggered) return;
+    let alert = document.createElement("div");
+    alert.classList.add("alert");
+    alert.innerHTML = "You don't have permission to move this task.";
+    document.body.appendChild(alert);
+    setTimeout(() => {
+        document.querySelector(".alert").remove();
+        alertTriggered = false;
+    }, 3000);
 }
