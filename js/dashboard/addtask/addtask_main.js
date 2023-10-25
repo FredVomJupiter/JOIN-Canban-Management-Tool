00d398b9b3ce9fd253f0a2974ec2f61ccc2e218f
Frederic Rieg <fredericrieg@protonmail.com>
2023-10-25 16:20:58 +0300

    implemented collection of form data for newTask

--- js/dashboard/addtask/addtask_main.js
+++ js/dashboard/addtask/addtask_main.js
@@ -1,3 +1,8 @@
+function renderAll() {
+    renderCategories();
+    renderAssignments();
+}
+
 /**
  * Clears the addtask form and resets the newTask object with default values.
  * Only the status of the task is preserved (if user comes from board btns).
@@ -22,12 +27,27 @@ function resetAddtaskInputs() {
  * Triggered by the addtask button.
  * Collects form data, validates it and sends the validated data to the Backend.
  */
-function collectValidateSendData() {
-    console.log("new task: ", newTask);
-}
-
-
-function renderAll() {
-    renderCategories();
-    renderAssignments();
+async function collectValidateSendData() {
+    newTask.title = document.getElementById('addtaskMenuTitle').value.trim();
+    newTask.description = document.getElementById('addtaskMenuDescription').value.trim();
+    newTask.category = document.getElementById('addtaskMenuCategory').value;
+    let selected = document.getElementById('addtaskMenuAssigned');
+    for (const option of selected.options) {
+        if (option.selected) {
+            newTask.assigned_to.push(option.value)
+        }
+    }
+    newTask.due_date = document.getElementById('addtaskMenuDate').value + "T00:00:00Z";
+    await Promise.all(newSubtasks.map(async subtask => {
+            let response = await createSubtask(subtask);
+            newTask.subtasks.push(response.id);
+    }));
+    let response = await createTask(newTask);
+    await getSubtasks();
+    await getTasks();
+    showAlert("Task created successfully.");
+    clearAddtaskMenu();
+    setTimeout(() => {
+        openPage('board');
+    }, 1000);
 }
