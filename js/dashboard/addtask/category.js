83ab3013f4d5d8064483a7eb08c96f415d842e30
Frederic Rieg <fredericrieg@protonmail.com>
2023-10-25 15:08:24 +0300

    implemented category creation in addtask

--- js/dashboard/addtask/category.js
+++ js/dashboard/addtask/category.js
@@ -1,12 +1,91 @@
+let stepCategory = 0;
+
 /**
  * Renders the categories as options in the add task section.
  */
 function renderCategories() {
+    let categoryList = document.getElementById('addtaskMenuCategory');
+    resetCategoriesList(categoryList);
     categories.forEach(category => {
-        let categoryList = document.getElementById('addtaskMenuCategory');
         let option = document.createElement('option');
         option.value = category.id;
         option.text = category.name + " (" + category.color + ")";
         categoryList.appendChild(option);
     });
+}
+
+
+function resetCategoriesList(categoryList) {
+    categoryList.innerHTML = "";
+    categoryList.innerHTML = `
+        <option value="">Select task category</option>
+        <option value="" onclick="showCreateCategory()">Create new category</option>
+        `;
+}
+
+
+function showCreateCategory() {
+    let createCategory = document.getElementById('addtaskMenuCategory');
+    createCategory.classList.add('d-none');
+    let createCategoryInput = document.getElementById('addtaskCreateCategory');
+    createCategoryInput.classList.remove('d-none');
+}
+
+
+function cancelCreateCategory() {
+    stepCategory = 0;
+    let createCategory = document.getElementById('addtaskMenuCategory');
+    createCategory.classList.remove('d-none');
+    let createCategoryInput = document.getElementById('addtaskCreateCategory');
+    createCategoryInput.classList.add('d-none');
+    document.getElementById('addtaskCreateCategoryInput').value = "";
+    document.getElementById('addtaskCreateCategoryInput').setAttribute('placeholder', 'Name');
+}
+
+
+function nextStepCategory() {
+    stepCategory++;
+    stepCategory === 1 ? collectCategoryName() : null;
+    stepCategory === 2 ? collectCategoryColor() : null;
+    stepCategory === 3 ? createCategoryInAddtask() : null;
+}
+
+
+function collectCategoryName() {
+    if (hasNoInputValueCat()) {
+        showAlert("Please enter a name.");
+        stepCategory--;
+        return;
+    }
+    let categoryName = document.getElementById('addtaskCreateCategoryInput').value;
+    newCategory.name = categoryName.trim();
+    newCategory.user = loggedUser;
+    document.getElementById('addtaskCreateCategoryInput').value = "";
+    document.getElementById('addtaskCreateCategoryInput').placeholder = "Enter a category color";
+}
+
+
+function collectCategoryColor() {
+    if (hasNoInputValueCat()) {
+        showAlert("Please enter a color.");
+        stepCategory--;
+        return;
+    }
+    let categoryColor = document.getElementById('addtaskCreateCategoryInput').value;
+    newCategory.color = categoryColor.trim();
+    createCategoryInAddtask();
+}
+
+
+function hasNoInputValueCat() {
+    return document.getElementById('addtaskCreateCategoryInput').value.trim() === "" ? true : false;
+}
+
+
+async function createCategoryInAddtask() {
+    await createCategory(newCategory);
+    await getCategories();
+    showAlert("Category created succesfully.");
+    renderCategories();
+    cancelCreateCategory();
 }
